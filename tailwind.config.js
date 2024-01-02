0cc77b6b5b0908321273f9a917c169c245885800
Vhs4 <vhszera44444@gmail.com>
2024-01-02 19:31:14 -0300

    FIX: Register error

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,28 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      'extraL2': {'max': '1535px'},
+      'extraL': {'max': '1279px'},
+      'large': {'max': '1023px'},
+      'medium': {'max': '767px'},
+      'small': {'max': '639px'},
+      'x-sm': {'max': '520px'},    
+    },
+    extend: {
+      colors: {
+        'customRed':'#B30733',
+        'customPink':'#FF0043',
+        'customOrange':'#EF4C00',
+        'customBG': '#F5F5F5',
+        'customLOrange': '#FD651F'
+      },
+    },
+  },
+  layers: {
+    utilities: [],
+    components: [],
+    base: [],
   },
   plugins: [],
 }
